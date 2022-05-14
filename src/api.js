50f507fbfdc05dc5e1e9f356a9227f385b330715
Harriet Blundell <58891140+Harriet-Blundell@users.noreply.github.com>
2022-05-14 17:53:23 +0100

    Set up fetchNextAvailableAppointmentsByDate and fetchAvailableAppointsmentsByDate

--- src/api.js
+++ src/api.js
@@ -1,4 +1,36 @@
 const { readFile } = require("fs").promises;
+const moment = require("moment");
+
+const fetchAllTherapists = (pageNumber) => {
+  return readFile("./data/counsellor-mock.json").then((therapistData) => {
+    const therapistObject = JSON.parse(therapistData);
+    return therapistObject.slice((pageNumber - 1) * 10, pageNumber * 10);
+  });
+};
+
+const fetchNextAvailableAppointmentsByDate = () => {
+  return readFile("./data/availability-mock.json").then((availabilityData) => {
+    let earliestDate = 0;
+    const nextAvailableDate = [];
+    const appointmentsObject = JSON.parse(availabilityData);
+    for (const therapist in appointmentsObject) {
+      earliestDate = moment(appointmentsObject[therapist][0].datetime).unix();
+      for (let i = 0; i < appointmentsObject[therapist].length; i++) {
+        const dateTime = moment(
+          appointmentsObject[therapist][i].datetime
+        ).unix();
+        if (dateTime < earliestDate) {
+          earliestDate = dateTime;
+        }
+      }
+      nextAvailableDate.push({
+        therapist,
+        nextAvailableTime: moment.unix(earliestDate).toDate(),
+      });
+    }
+    return nextAvailableDate;
+  });
+};
 
 const fetchTherapistById = (id) => {
   return readFile("./data/counsellor-mock.json").then((data) => {
@@ -9,3 +41,45 @@ const fetchTherapistById = (id) => {
     return filteredData;
   });
 };
+
+export const fetchAvailableAppointmentsByDate = (
+  earliestDateInput,
+  latestDateInput
+) => {
+  const earliestDate = moment(earliestDateInput).unix();
+  const latestDate = moment(latestDateInput).unix();
+
+  return readFile("./data/availability-mock.json").then(
+    (appointmentAvailabilityData) => {
+      const appointmentsObject = JSON.parse(appointmentAvailabilityData);
+      for (const therapist in appointmentsObject) {
+        for (let i = 0; i < appointmentsObject[therapist].length; i++) {
+          const availableAppointment = moment(
+            appointmentsObject[therapist][i].datetime
+          ).unix();
+          if (
+            availableAppointment > earliestDate &&
+            availableAppointment < latestDate
+          ) {
+            // TODO
+          }
+        }
+      }
+    }
+  );
+};
+
+//fetch therapists by specific type.
+//fetch therapists by specialisation.
+
+/*
+- use a for-in loop to access the key of each therapist in the object
+- use a for loop to iterate through the key values of each individual therapist by using bracket notation
+- use dot notation to access the datetime key value in the object
+- turn the datetime into unix time
+- sessions start at 6am until 9pm
+
+- if I choose 09:00 between 26th and 28th May
+
+- In front end, take calendar input from user and convert to unix time to be used by fetch function comparison.
+*/
