ddd6bb8ba3dc0ac623c0be3cf1568153a5c24833
Harriet Blundell <58891140+Harriet-Blundell@users.noreply.github.com>
2022-05-19 20:44:02 +0100

    Begin code and styling of therapist data

--- src/TherapistCard.js
+++ src/TherapistCard.js
@@ -1,7 +1,36 @@
+import "./TherapistCard.css";
 import React from "react";
 
-export default function TherapistCard(props) {
-  console.log(props, "<<<<");
-
-  return <div>Hello</div>;
+export default function TherapistCard({
+  id,
+  firstName,
+  lastName,
+  apointment_mediums,
+  appointment_types,
+  specialisms,
+}) {
+  return (
+    <div className="therapist-card-container">
+      <div class="therapist-content">
+        <h3>
+          {firstName} {lastName}
+        </h3>
+        <p>Therapist</p>
+        <p>
+          Appointment types: {appointment_types[0]} and {appointment_types[1]}
+        </p>
+        <div className="specialism-content">
+          <div>
+            Can help you with:
+            {specialisms.length !== 0 ? specialisms.map((specialism) => {
+              return <span> {specialism}, </span>;
+            }) : " N/A"}
+          </div>
+        </div>
+      </div>
+      <div className="button-container">
+        <button>Book Now</button>
+      </div>
+    </div>
+  );
 }
