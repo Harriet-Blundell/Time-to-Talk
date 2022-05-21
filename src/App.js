2e35d925e4138c5ef34af768437615ad94bc499a
Harriet Blundell <58891140+Harriet-Blundell@users.noreply.github.com>
2022-05-21 10:52:17 +0100

    Refactor code
    Returning undefined

--- src/App.js
+++ src/App.js
@@ -5,24 +5,33 @@ import { fetchAllTherapists } from "./api";
 import TherapistCard from "./TherapistCard";
 
 function App() {
-  const [allTherapists, setAllTherapists] = useState([]);
+  const [allTherapists, setAllTherapists] = useState(null);
 
   useEffect(() => {
     setAllTherapists({
-      allTherapists: fetchAllTherapists(1)
-    })
+      allTherapists: fetchAllTherapists(1),
+    });
   }, []);
 
   return (
     <>
       <Header />
       <div className="therapist-container">
-      {allTherapists.allTherapists.map((therapist) => {
-        return <TherapistCard {...therapist} />
-      })}
+        {allTherapists
+          ? allTherapists.allTherapists.map((therapist) => {
+              return <TherapistCard {...therapist} />;
+            })
+          : ""}
       </div>
     </>
   );
 }
 
 export default App;
+
+/*
+- A ternary operator had to be used on 'allTherapists' because the function was being called
+  before the DOM was ready therefore the data was not loading and showing an error in the console as "undefined"
+- This allows there to be an empty string for the time being until the DOM is ready and then the function is called
+
+*/
--- src/TherapistCard.js
+++ src/TherapistCard.js
@@ -5,32 +5,51 @@ export default function TherapistCard({
   id,
   firstName,
   lastName,
-  apointment_mediums,
+  appointment_mediums,
   appointment_types,
   specialisms,
 }) {
   return (
     <div className="therapist-card-container">
-      <div class="therapist-content">
-        <h3>
-          {firstName} {lastName}
-        </h3>
-        <p>Therapist</p>
+      <div className="therapist-information-container">
+        <div className="therapist-initials">
+          <h1>
+            {firstName[0]}
+            {lastName[0]}
+          </h1>
+        </div>
+        <div className="therapist-information">
+          <h3>
+            {firstName} {lastName}
+          </h3>
+          <p>Therapist</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+/*
+Button component:
+- I would need to pass props down to the component such as id, firstName, lastName, appointment_type, appointment_medium, and availability
+- The information would need to be passed to "Confirmation" component screen
+<p>Therapist</p>
         <p>
           Appointment types: {appointment_types[0]} and {appointment_types[1]}
         </p>
         <div className="specialism-content">
           <div>
             Can help you with:
-            {specialisms.length !== 0 ? specialisms.map((specialism) => {
-              return <span> {specialism}, </span>;
-            }) : " N/A"}
+            {specialisms.length !== 0
+              ? specialisms.map((specialism) => {
+                  return <span> {specialism}, </span>;
+                })
+              : " N/A"}
           </div>
         </div>
       </div>
       <div className="button-container">
         <button>Book Now</button>
       </div>
-    </div>
-  );
-}
+
+*/
