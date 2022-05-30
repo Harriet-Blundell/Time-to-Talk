28f2f7531702f097070ad7068b9e22a5d9eca569
Harriet Blundell <58891140+Harriet-Blundell@users.noreply.github.com>
2022-05-30 19:57:07 +0100

    Add button logic and styling

--- src/Pagination.js
+++ src/Pagination.js
@@ -1,23 +1,30 @@
 import React from "react";
+import "./Pagination.css";
 
 export default function Pagination(props) {
   const { currentPage, handlePageClick } = props;
   return (
-    <div>
+    <div className="button-container">
       <button
+        className="button"
         disabled={currentPage === 1}
         onClick={() => {
           handlePageClick(-1);
         }}
       >
-        back
+        {currentPage === 1 ? (
+          <img src="chevron-left.png" alt="left chevron" />
+        ) : (
+          <img src="chevron-left-active.png" alt="left chevron" />
+        )}
       </button>
       <button
+        className="button"
         onClick={() => {
           handlePageClick(1);
         }}
       >
-        forward
+        <img src="chevron-right.png" alt="right chevron" />
       </button>
     </div>
   );
