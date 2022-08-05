d2bdff8bc9baf31288a06f6abcf603afc90dad7c
Harriet Blundell <58891140+Harriet-Blundell@users.noreply.github.com>
2022-08-05 17:26:04 +0100

    Update BookingMedium.jsx
    Refactor format of file

--- src/Components/BookingMedium.jsx
+++ src/Components/BookingMedium.jsx
@@ -1,20 +1,36 @@
 import React from "react";
-import '../styles/BookingMedium.css';
+import "../styles/BookingMedium.css";
 
 export default function BookingMedium() {
   return (
     <div class="booking-medium-container">
-        <div class="video-option-container">
-        <input type="checkbox" id="video-call" name="video-call" value="Video Call" class="video-checkbox" />
-        <img src="video-camera.png" alt="video camera icon" class="video-camera-img"/>
+      <div class="video-option-container">
+        <input
+          type="checkbox"
+          id="video-call"
+          name="video-call"
+          value="Video Call"
+          class="video-checkbox"
+        />
+        <img
+          src="video-camera.png"
+          alt="video camera icon"
+          class="video-camera-img"
+        />
         <label for="video-call">Video Call</label>
-        </div>
+      </div>
 
-        <div class="phone-option-container">
-        <input type="checkbox" id="phone-call" name="phone-call" value="Phone" class="phone-checkbox" />
-        <img src="call-phone.png" alt="phone icon" class="phone-img"/>
+      <div class="phone-option-container">
+        <input
+          type="checkbox"
+          id="phone-call"
+          name="phone-call"
+          value="Phone"
+          class="phone-checkbox"
+        />
+        <img src="call-phone.png" alt="phone icon" class="phone-img" />
         <label for="phone-call">Phone</label>
-        </div>
+      </div>
     </div>
   );
 }
